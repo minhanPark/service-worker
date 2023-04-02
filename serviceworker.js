80c6ed22596c3917d64835ef2c828fc077c5ca2f
Running water <alsgks1218@naver.com>
2023-04-02 23:56:40 +0900

    등록 및 fetch

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,33 @@
-addEventListener("fetch", function (event) {
-  console.log("서비스 워커가 이벤트를 감시합니다.");
+const staticCacheName = "staticFiles";
+
+addEventListener("install", (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(staticCacheName).then((staticCache) => {
+      staticCache.addAll([
+        //캐시 권장 파일
+      ]);
+      return staticCache.addAll([
+        // 캐시 필수 파일
+      ]);
+    })
+  );
+});
+
+addEventListener("activate", function (event) {
+  console.log("서비스 워커를 활성화되었습니다.");
+});
+
+addEventListener("fetch", (fetchEvent) => {
+  const request = fetchEvent.request;
+  fetchEvent.respondWith(
+    fetch(request)
+      .then((responseFromFetch) => {
+        return responseFromFetch;
+      })
+      .catch((error) => {
+        return new Response("<h1>이런 !</h1> <p>뭔가가 잘못되었습니다.</p>", {
+          headers: { "Content-Type": "text/html; charset=utf-8" },
+        });
+      })
+  );
 });
